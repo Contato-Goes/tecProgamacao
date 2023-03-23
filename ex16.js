cb3e7cc7e0b7e2ad0fa69ef303eb932aa748f6c4
Auto Logon <autologon@CSED.LAB>
2023-03-23 09:11:06 -0300

    Adicionando outro exemplo e criando funções

--- ex16.js
+++ ex16.js
@@ -10,8 +10,13 @@ class Livro{
     get Preco(){return this.preco}
     set Preco(pPreco) {this.preco = pPreco}
 
+    calcularDesconto(){
+        this.preco = this.preco - 10;
+    }
+
 }
 
 var obj_livro1 = new Livro("Use a cabeça: Java", 200);
+obj_livro1.calcularDesconto();
 console.log("Nome do livro " + obj_livro1.nome)
 console.log("Preço do livro R$" + obj_livro1.preco)
